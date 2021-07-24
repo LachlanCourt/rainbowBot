1b118a9290438267ed9ef3e84fd8bd839e4078d9
LachlanCourt <80299053+LachlanCourt@users.noreply.github.com>
2021-07-24 10:05:01 +1000

    Refactor of lock command (#23)
    * Add locked.dat to .gitignore
    
    * Condense checkPerms into a single function
    
    * Solve issue of lock command being treated as a deleted message

--- bot.py
+++ bot.py
@@ -56,22 +56,17 @@ for i in reportingChannelsList:
     reportingChannels[i[0]] = [i[1], i[2]]
 
 # Only discord users with a role in the trustedRoles list will be allowed to use bot commands    
-def checkPerms(msg):
+def checkPerms(msg, reaction=False):
+    if reaction == True:
+        user = msg
+    else:
+        user = msg.message.author
     roleNames = []
-    for i in range(len(msg.message.author.roles)):
-        roleNames.append(msg.message.author.roles[i].name)
+    for i in range(len(user.roles)):
+        roleNames.append(user.roles[i].name)
     if any(i in roleNames for i in trustedRoles):
         return True
     return False
-
-def checkPermsReaction(member):
-    roleNames = []
-    for i in range(len(member.roles)):
-        roleNames.append(member.roles[i].name)
-    if any(i in roleNames for i in trustedRoles):
-        return True
-    return False
-
 
 @client.event
 async def on_ready():
@@ -107,8 +102,8 @@ async def on_message(message):
 async def on_raw_message_delete(rawMessage):
     guild = client.get_guild(rawMessage.guild_id)
     channel = client.get_channel(rawMessage.channel_id)
-    # If the message was sent before the bot was logged on, it is unfortunately innaccessible. Ignore also if the author is on the whitelist
-    if not rawMessage.cached_message or channel.name in reportingChannels or rawMessage.cached_message.author.name in whitelist:
+    # If the message was sent before the bot was logged on, it is unfortunately innaccessible. Ignore also if the author is on the whitelist or if the channel is locked (The lock channel command deletes the message of the sender automatically)
+    if not rawMessage.cached_message or channel.name in reportingChannels or rawMessage.cached_message.author.name in whitelist or channel.name in lockedChannels:
         return    
     message = rawMessage.cached_message
     member = guild.get_member(message.author.id)
@@ -200,7 +195,7 @@ async def on_raw_reaction_add(reaction):
 
         if role == None:
             return
-        if reaction.emoji.name == "🔓" and checkPermsReaction(reaction.member):
+        if reaction.emoji.name == "🔓" and checkPerms(reaction.member, reaction=True):
             await msg.channel.set_permissions(role, read_messages=True, send_messages=None)
             lockedChannels.remove(msg.channel.name)
             data = {'channels':lockedChannels}
@@ -558,9 +553,9 @@ async def lock(msg, *args):
         await msg.channel.send("Channel can not be locked")
         return
 
+    lockedChannels.append(msg.channel.name)
     await msg.message.delete(delay=None)
     await msg.channel.set_permissions(role, read_messages=True, send_messages=False)
-    lockedChannels.append(msg.channel.name)
     data = {'channels':lockedChannels}
 
     channel = await msg.channel.send("Channel locked! React with trusted permissions to unlock!")
