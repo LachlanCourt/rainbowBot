b5de244d733bedb90e1da40c861717e679c67d8f
LachlanCourt <c3308061@uon.edu.au>
2021-09-22 12:06:07 +1000

    Add function call on timer

--- cogs/Tasks.py
+++ cogs/Tasks.py
@@ -1,5 +1,6 @@
 import discord, json
 from discord.ext import commands
+from discord.ext import tasks
 
 from cogs.Moderation import Moderation
 
@@ -8,7 +9,12 @@ class Tasks(commands.Cog):
     def __init__(self, client, config):
         self.client = client
         self.config = config
+        self.printer.start()
 
-    @commands.command("test")
-    async def test(self, msg):
-        await Moderation.lock(self, msg, "comp1000")
+##    @commands.command("test")
+##    async def test(self, msg):
+##        await Moderation.lock(self, msg, "comp1000")
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print("tick")
