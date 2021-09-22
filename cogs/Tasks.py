de0dad345acf82bab282eee2e41c81345ca4fea2
LachlanCourt <c3308061@uon.edu.au>
2021-09-22 18:26:33 +1000

    Add function to determine if current time matches a task

--- cogs/Tasks.py
+++ cogs/Tasks.py
@@ -1,4 +1,4 @@
-import discord, json
+import discord, json, datetime
 from discord.ext import commands
 from discord.ext import tasks
 
@@ -11,10 +11,64 @@ class Tasks(commands.Cog):
         self.config = config
         self.printer.start()
 
+    @staticmethod
+    def isNow(cronStamp):
+        now = datetime.datetime.now()
+        cronStamp = cronStamp.split()
+        
+        # Minutes
+        if cronStamp[0] != "*":
+            nowMins = int(now.strftime("%M"))
+            if int(cronStamp[0]) != nowMins:
+                return False
+        # Hour
+        if cronStamp[1] != "*":
+            nowHours = int(now.strftime("%H"))
+            if int(cronStamp[1]) != nowHours:
+                return False
+
+        # Date
+        if cronStamp[2] != "*":
+            nowDate = int(now.strftime("%d"))
+            if int(cronStamp[2]) != nowDate:
+                return False
+
+        # Month
+        if cronStamp[3] != "*":
+            nowMonth = int(now.strftime("%m"))
+            if int(cronStamp[3]) != nowMonth:
+                return False
+
+        # Day
+        if cronStamp[4] != "*":
+            nowDay = int(now.strftime("%w"))
+            if int(cronStamp[4]) != nowDay:
+                return False
+
+        return True
+
 ##    @commands.command("test")
 ##    async def test(self, msg):
 ##        await Moderation.lock(self, msg, "comp1000")
 
-    @tasks.loop(seconds=5.0)
+    
+    @tasks.loop(seconds=5.0) # Change to minutes
     async def printer(self):
-        print("tick")
+        # Reads file in the same format as crontab
+        # Minute Hour Date Month Day
+        f = open("tasks.json")
+        data = json.load(f)
+        f.close()
+        tasks = data["tasks"]
+
+        for i in tasks:
+            if isNow(i[0]):
+                if i[1] == "lock":
+                    # Generate message object
+
+                    # Get log channel
+                    # Send lock message
+                    # Save returned message
+                    Moderation.lock(self, message, i[2])
+        
+        print(tasks)
