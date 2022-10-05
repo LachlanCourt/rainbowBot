8fd31ba72e26b0d2e477d2a1876ef7766048d929
LachlanCourt <lachlan.m.court@gmail.com>
2022-10-05 15:17:04 +1100

    Left pad single digit dates and times with zeros

--- cogs/helpers/_taskValidator.py
+++ cogs/helpers/_taskValidator.py
@@ -45,12 +45,12 @@ class Validator:
                 args = task[2]
                 preposition = task[3]
                 end = task[4].split()
-                out += f"Execute command `{command} {args}` at {start[1] if start[1] != '*' else 'any'}:{start[0] if start[0] != '*' else 'any'}"
-                out += f" on day {start[2] if start[2] != '*' else 'any'} of {months[start[3]]} if it is {days[start[4]]}day\n"
+                out += f"Execute command `{command} {args}` at {start[1].zfill(2) if start[1] != '*' else 'any'}:{start[0].zfill(2) if start[0] != '*' else 'any'}"
+                out += f" on day {start[2].zfill(2) if start[2] != '*' else 'any'} of {months[start[3]]} if it is {days[start[4]]}day\n"
 
                 if preposition == "until":
-                    out += f"    Revert command `{command} {args}` at {end[1] if end[1] != '*' else 'any'}:{end[0] if end[0] != '*' else 'any'}"
-                    out += f" on day {end[2] if end[2] != '*' else 'any'} of {months[end[3]]} if it is {days[end[4]]}day\n\n"
+                    out += f"    Revert command `{command} {args}` at {end[1].zfill(2) if end[1] != '*' else 'any'}:{end[0].zfill(2) if end[0] != '*' else 'any'}"
+                    out += f" on day {end[2].zfill(2) if end[2] != '*' else 'any'} of {months[end[3]]} if it is {days[end[4]]}day\n\n"
 
                 # Add custom EOL character to split on in output command
                 out += chr(255)
